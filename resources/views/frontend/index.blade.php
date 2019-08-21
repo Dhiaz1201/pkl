85852b3e5d380757cf44c998316694875eea93ad
diaz <mulyanaiyan11@gmail.com>
2019-08-21 23:15:50 +0700

    tambah

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -57,11 +57,9 @@
             <div class="single_post_content_left">
               <ul class="business_catgnav  wow fadeInDown">
                 <li>
-                  <figure class="bsbig_fig"> <a href="pages/single_page.html" class="featured_img">  <img src="{{ asset('assets/img/artikel/'.$data->foto) }} "width="100px" height="300px"> <span class="overlay"></span> </a>
-                  <div class="slider_article">
-                    <figcaption><a class="slider_tittle" href="pages/single_page.html">{{ $data->judul }}</a> </figcaption>
+                   <figure class="bsbig_fig"> <a href="pages/single_page.html" class="featured_img"> <img src="{{ asset('assets/img/artikel/'.$data->foto) }}" alt="" height="200px" width="300px"> <span class="overlay"></span> </a>
+                    <figcaption> <a href="pages/single_page.html">{{ $data->judul }}</a> </figcaption>
                     <p>{{ $data->konten }}</p>
-                    </div>
                   </figure>
                 </li>
               </ul>
